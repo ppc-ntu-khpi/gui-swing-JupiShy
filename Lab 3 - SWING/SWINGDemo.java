de5de1d992ad0e1ef111ca776c5328b0d05626a0
Sofiia Kyrychok <101671647+JupiShy@users.noreply.github.com>
2024-06-07 12:11:24 +0300

    Add files via upload

--- Lab 3 - SWING/SWINGDemo.java
+++ Lab 3 - SWING/SWINGDemo.java
@@ -1,14 +1,19 @@
-package com.mybank.gui;
+//package com.mybank.gui;
 
+import com.mybank.domain.Account;
 import com.mybank.domain.Bank;
 import com.mybank.domain.CheckingAccount;
 import com.mybank.domain.Customer;
 import com.mybank.domain.SavingsAccount;
+import com.mybank.reporting.CustomerReport;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JEditorPane;
@@ -23,12 +28,14 @@ public class SWINGDemo {
     
     private final JEditorPane log;
     private final JButton show;
+    private final JButton report;
     private final JComboBox clients;
     
     public SWINGDemo() {
         log = new JEditorPane("text/html", "");
-        log.setPreferredSize(new Dimension(250, 150));
+        log.setPreferredSize(new Dimension(250, 600));
         show = new JButton("Show");
+        report = new JButton("Report");
         clients = new JComboBox();
         for (int i=0; i<Bank.getNumberOfCustomers();i++)
         {
@@ -45,6 +52,7 @@ public class SWINGDemo {
         
         cpane.add(clients);
         cpane.add(show);
+        cpane.add(report);
         frame.add(cpane, BorderLayout.NORTH);
         frame.add(log, BorderLayout.CENTER);
         
@@ -61,21 +69,82 @@ public class SWINGDemo {
             }
         });
         
+        report.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+               
+                String title = "CUSTOMERS REPORT";
+                String customers = "";
+                String customerName = "";
+                String account_type = "";          
+    
+    for ( int cust_idx = 0;
+          cust_idx < Bank.getNumberOfCustomers();
+          cust_idx++ ) {
+      Customer customer = Bank.getCustomer(cust_idx);
+      customerName = customer.getFirstName() + ", " + customer.getLastName();
+     
+      for ( int acct_idx = 0;
+            acct_idx < customer.getNumberOfAccounts();
+            acct_idx++ ) {
+        Account account = customer.getAccount(acct_idx);
+        
+        if ( account instanceof SavingsAccount ) {
+          account_type = "Savings Account";
+        } else if ( account instanceof CheckingAccount ) {
+          account_type = "Checking Account";
+        } else {
+          account_type = "Unknown Account Type";
+        }
+
+        customers += "<b>" + customerName + "<br>Acc type: </b>" + account_type + "<br><b>Balance: </b> $" + account.getBalance() + "<br><hr>";
+      }
+    }
+        String report = "<br>&nbsp;<b><span style=\"font-size:2em;\">"+title+"</span><br><hr><br>" +customers+ "";
+        log.setText(report);  
+            }
+        });
+        
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-        frame.setResizable(false);
+        frame.setResizable(true);
         frame.setVisible(true);        
     }
     
     public static void main(String[] args) {
-        
-        Bank.addCustomer("John", "Doe");
-        Bank.addCustomer("Fox", "Mulder");
-        Bank.addCustomer("Dana", "Scully");
-        Bank.getCustomer(0).addAccount(new CheckingAccount(2000));
-        Bank.getCustomer(1).addAccount(new SavingsAccount(1000, 3));
-        Bank.getCustomer(2).addAccount(new CheckingAccount(1000, 500));
+                
+        try (BufferedReader br = new BufferedReader(new FileReader("test.dat"))) {
+            int numberOfCustomers = Integer.parseInt(br.readLine());
+            for (int i = 0; i < numberOfCustomers; i++) {
+                br.readLine();
+                String[] customerInfo = br.readLine().split("\t");
+
+                Bank.addCustomer(customerInfo[0], customerInfo[1]);
+                
+                int numberOfAccounts = Integer.parseInt(customerInfo[2]);
+                
+                Customer customer = Bank.getCustomer(i);
+
+                for (int j = 0; j < numberOfAccounts; j++) {
+                    String[] accountInfo = br.readLine().split("\t");
+                    String accountType = accountInfo[0];
+                    double balance = Double.parseDouble(accountInfo[1]);
+                    switch (accountType) {
+                        case "S":
+                            double interestRate = Double.parseDouble(accountInfo[2]);
+                            customer.addAccount(new SavingsAccount(balance, interestRate));
+                            break;
+                        case "C":
+                            double overdraftAmount = Double.parseDouble(accountInfo[2]);
+                            customer.addAccount(new CheckingAccount(balance, overdraftAmount));
+                            break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         
         SWINGDemo demo = new SWINGDemo();        
         demo.launchFrame();
